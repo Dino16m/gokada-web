f3678c4c225082ee209f03ee39e5d3d9ab53c88c
dino16m <anselem16m@gmail.com>
2021-04-10 02:15:20 +0100

    add built preview

---
+++ docs/precache-manifest.5583288b59d6a5840eedc1e4603bc3a6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb383d085b12cb19c53f",
+    "url": "/gokada-web/css/app.b562549c.css"
+  },
+  {
+    "revision": "0000937e0f81eb30aff3",
+    "url": "/gokada-web/css/chunk-vendors.01ae1b91.css"
+  },
+  {
+    "revision": "05250ff0fd8d1e9b81487e610322dd4e",
+    "url": "/gokada-web/img/dropoff.png"
+  },
+  {
+    "revision": "2273e3d8ad9264b7daa5bdbf8e6b47f8",
+    "url": "/gokada-web/img/marker-icon.png"
+  },
+  {
+    "revision": "44a526eed258222515aa21eaffd14a96",
+    "url": "/gokada-web/img/marker-shadow.png"
+  },
+  {
+    "revision": "3d755970d4fdbf80297772766c30abe5",
+    "url": "/gokada-web/img/pickup.png"
+  },
+  {
+    "revision": "15d147ea15dae4ea60daf8b137a03234",
+    "url": "/gokada-web/index.html"
+  },
+  {
+    "revision": "ecc9f6c4e8eb5c1d65ea",
+    "url": "/gokada-web/js/about.f2f20e38.js"
+  },
+  {
+    "revision": "cb383d085b12cb19c53f",
+    "url": "/gokada-web/js/app.4120898b.js"
+  },
+  {
+    "revision": "0000937e0f81eb30aff3",
+    "url": "/gokada-web/js/chunk-vendors.119931de.js"
+  },
+  {
+    "revision": "b80b4bd01985d28b8db560f4ab9fde33",
+    "url": "/gokada-web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/gokada-web/robots.txt"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "gokada/precache-manifest.17b79faa150660224f172adbfdcef2b3.js"
+  "/gokada-web/precache-manifest.5583288b59d6a5840eedc1e4603bc3a6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "gokada-client"});
